f7c612df0d2bd123ef87eedc22f58f68225136e8
Unseed <unseedevil@gmail.com>
2023-01-15 09:29:40 +0300

    Add all

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,3 +1,27 @@
 ﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.Write("Input M: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Input N: ");
+int N = int.Parse(Console.ReadLine());
+
+int count = print(M, N);
+
+Console.WriteLine(count);
+
+int print(int M, int N)
+{
+    if (M <= N)
+    {
+        return M + print(M + 1, N);
+    }
+    else
+    {
+        return 0;
+    }
+}
--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,3 +1,36 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+
+int A = print(m, n);
+
+Console.WriteLine($"A(m,n) = {A}");
+
+int print(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    
+    else
+    {
+        if (n != 0 && m == 0)
+        {
+            return print(n - 1, 1);
+        }
+        else
+        {
+            return print(n - 1, print(n, m - 1));
+        }
+    }
+    
+}
